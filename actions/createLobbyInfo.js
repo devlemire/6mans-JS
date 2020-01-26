f5424721b7d19b2a9d4955022434ea56718911b0
James Lemire <webdev.jameslemire@gmail.com>
2020-01-26 17:28:18 -0500

    Add a random structure message. Add a channel.send lobby ready message

--- actions/createLobbyInfo.js
+++ actions/createLobbyInfo.js
@@ -3,35 +3,38 @@ const { deletePlayerQueue } = require('../utils/managePlayerQueues')
 
 module.exports = (eventObj, queue) => {
   const { teams, lobby } = queue
+  const channel = eventObj.author.lastMessage.channel
 
   const randomTeam = randomNumber(1) === 0 ? 'blue' : 'orange'
   const randomPlayer = randomNumber(2)
 
   const creatorId = teams[randomTeam].players[randomPlayer].id
 
+  const readyEmbed = {
+    embed: {
+      color: 2201331,
+      title: `Lobby ${lobby.name} - Ready`,
+      description: 'Please join the correct voice channel based on your team',
+      fields: [
+        {
+          name: 'Blue',
+          value: teams.blue.players.map(playerObj => `<@${playerObj.id}>`).join(', '),
+        },
+        {
+          name: 'Orange',
+          value: teams.orange.players.map(playerObj => `<@${playerObj.id}>`).join(', '),
+        },
+        {
+          name: 'Creates the lobby',
+          value: `<@${creatorId}>`,
+        },
+      ],
+    },
+  }
+
   function sendLobbyInfo(players) {
     players.forEach(player => {
-      player.dmPlayer({
-        embed: {
-          color: 2201331,
-          title: `Lobby ${lobby.name} - Ready`,
-          description: 'Please join the correct voice channel based on your team',
-          fields: [
-            {
-              name: 'Blue',
-              value: teams.blue.players.map(playerObj => `<@${playerObj.id}>`).join(', '),
-            },
-            {
-              name: 'Orange',
-              value: teams.orange.players.map(playerObj => `<@${playerObj.id}>`).join(', '),
-            },
-            {
-              name: 'Creates the lobby',
-              value: `<@${creatorId}>`,
-            },
-          ],
-        },
-      })
+      player.dmPlayer(readyEmbed)
 
       player.dmPlayer({
         embed: {
@@ -53,7 +56,14 @@ module.exports = (eventObj, queue) => {
     })
   }
 
+  // DM Blue Team
   sendLobbyInfo(teams.blue.players)
+  // DM Orange Team
   sendLobbyInfo(teams.orange.players)
+
+  // Inform the channel that everything is ready
+  channel.send(readyEmbed)
+
+  // Delete the player queue
   deletePlayerQueue(lobby.id)
 }
--- actions/createRandomTeams.js
+++ actions/createRandomTeams.js
@@ -2,21 +2,34 @@ const randomNumber = require('../utils/randomNumber')
 const createLobbyInfo = require('./createLobbyInfo')
 
 module.exports = (eventObj, queue) => {
-  const { players, teams } = queue
+  const { lobby, players, teams } = queue
+  const channel = eventObj.author.lastMessage.channel
   queue.votingInProgress = false
   queue.creatingTeamsInProgress = true
 
+  // Tell the server that random mode was chosen
+  channel.send({
+    embed: {
+      color: 2201331,
+      title: `Lobby ${lobby.name} - Random structure`,
+      description: 'The vote resulted in random structure. You will receive a DM when the teams are automatially created.',
+    },
+  })
+
+  // Create blue team
   while (teams.blue.players.length !== 3) {
     const randomIndex = randomNumber(players.length - 1)
     teams.blue.players.push(players[randomIndex])
     players.splice(randomIndex, 1)
   }
 
+  // Create orange team
   while (teams.orange.players.length !== 3) {
     const randomIndex = randomNumber(players.length - 1)
     teams.orange.players.push(players[randomIndex])
     players.splice(randomIndex, 1)
   }
 
+  // Create the lobby
   createLobbyInfo(eventObj, queue)
 }
