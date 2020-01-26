0ed2a914607d04232ebba13f755daf05b39cad95
James Lemire <webdev.jameslemire@gmail.com>
2020-01-26 12:46:42 -0500

    Fix exports... again lol

--- utils/commands.js
+++ utils/commands.js
@@ -1,19 +1,18 @@
-module.exports = {
-  commandToString: {
-    queue: '!6m-q',
-    leave: '!6m-leave',
-    status: '!6m-status',
-    votestatus: '!6m-votestatus',
-    r: '!6m-r',
-    c: '!6m-c',
-    help: '!6m-help',
-  },
-  validCommandCheck: {
-    '!6m-q': true,
-    '!6m-leave': true,
-    '!6m-status': true,
-    '!6m-votestatus': true,
-    '!6m-r': true,
-    '!6m-c': true,
-  },
+exports.commandToString = {
+  queue: '!6m-q',
+  leave: '!6m-leave',
+  status: '!6m-status',
+  votestatus: '!6m-votestatus',
+  r: '!6m-r',
+  c: '!6m-c',
+  help: '!6m-help',
+}
+
+exports.validCommandCheck = {
+  '!6m-q': true,
+  '!6m-leave': true,
+  '!6m-status': true,
+  '!6m-votestatus': true,
+  '!6m-r': true,
+  '!6m-c': true,
 }
