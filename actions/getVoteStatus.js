d020eb3795ee6f07a7792d923c1cf157137f2af8
James Lemire <webdev.jameslemire@gmail.com>
2020-01-28 08:05:26 -0500

    Allow gitm-votestatus to be called during any phase of the queue

--- actions/getVoteStatus.js
+++ actions/getVoteStatus.js
@@ -10,14 +10,14 @@ module.exports = (eventObj, queue) => {
   if (playerNotInQueue({ playerId, channel, queue })) return
 
   // Player is in the queue
-  if (!votingInProgress) {
-    // Voting is not in progress
-    return channel.send(`You cannot vote because the voting phase is not in progress <@${playerId}>`)
+  if (!votingInProgress && !creatingTeamsInProgress && !readyToJoin) {
+    // Voting has not started yet
+    return channel.send(`The voting phase has not started yet <@${playerId}>`)
   }
 
-  // Voting is in progress
+  // Voting has started already
   // Get a list of mentions for the players who haven't voted
-  const playersWhoHaventVoted = Object.keys(playerIdsIndexed)
+  let playersWhoHaventVoted = Object.keys(playerIdsIndexed)
     .map(playerId => {
       if (!votes.playersWhoVoted[playerId]) {
         return `<@${playerId}>`
@@ -27,13 +27,21 @@ module.exports = (eventObj, queue) => {
     })
     .filter(mentionString => mentionString !== undefined)
 
+  if (playersWhoHaventVoted.length > 0) {
+    // Display the players who need to vote
+    playersWhoHaventVoted.join(', ')
+  } else {
+    // Display blank - No player needs to vote
+    playersWhoHaventVoted = ''
+  }
+
   channel.send({
     embed: {
       color: 2201331,
       title: `Lobby ${lobby.name} - Vote status`,
       description: `${remainingVotesRequired} votes remaining`,
       fields: [
-        { name: 'Needs to Vote', value: playersWhoHaventVoted.join(', ') },
+        { name: 'Needs to Vote', value: playersWhoHaventVoted },
         { name: 'Random Teams', value: votes.r, inline: true },
         { name: 'Captains', value: votes.c, inline: true },
       ],
